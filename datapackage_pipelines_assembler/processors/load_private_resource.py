b8784fe903c05ec979345e752327dfd234f42771
zelima <irakli.mchedlishvili@datopian.com>
2017-11-23 12:57:52 +0400

    modify resource object in as impler way

--- datapackage_pipelines_assembler/processors/load_private_resource.py
+++ datapackage_pipelines_assembler/processors/load_private_resource.py
@@ -1,4 +1,3 @@
-import datapackage
 from datapackage_pipelines.wrapper import ingest
 from datapackage_pipelines.lib.load_resource import ResourceLoader
 from planner.utilities import s3_path
@@ -11,19 +10,12 @@ class PrivateResourceLoader(ResourceLoader):
         self.parameters, self.dp, self.res_iter = ingest()
 
     def process_datapackage(self, dp_):
-        cached_paths = {}
-        for i, res in enumerate(dp_.resources):
-            # res.source = signed-url throws AttributError: can't set attribute
-            # So we need to create new DataPackage object with resource path=signed-url
-            # And modify res.source that way.
-            cached_paths[i] = res.descriptor['path']
-            dp_.descriptor['resources'][i]['path'] = s3_path(res.source)
-
-        modified_dp = datapackage.DataPackage(dp_.descriptor)
-        for i, res in enumerate(modified_dp.resources):
-            # We want back original paths. This can be modified directly
-            res.descriptor['path'] = cached_paths[i]
-        return modified_dp
+        for res in dp_.resources:
+            cached_path = res.descriptor['path']
+            res.descriptor['path'] = s3_path(res.source)
+            res.commit()
+            res.descriptor['path'] = cached_path  # Keep original paths
+        return dp_
 
 
 if __name__ == '__main__':
