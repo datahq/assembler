e07a67d7462ed7129dc44eaa5b7c1b1bf7e6f640
zelima <irakli.mchedlishvili@datopian.com>
2017-11-27 15:51:58 +0400

    [data validation][m]: processor for data validation -refs #71

---
+++ datapackage_pipelines_assembler/processors/validate_resource.py
@@ -0,0 +1,44 @@
+import json
+
+from datapackage import Package
+from goodtables import validate
+from datapackage_pipelines.wrapper import spew, ingest
+from datapackage_pipelines.utilities.resources import PROP_STREAMED_FROM
+import logging
+
+parameters, datapackage, res_iter = ingest()
+
+report_name = 'datapackage_report.json'
+
+def generate_report(datapackage_):
+    reports = []
+    dp = Package(datapackage_)
+
+    for resource in dp.resources:
+        report = validate(
+            resource.descriptor[PROP_STREAMED_FROM],
+            preset='datapackage',
+            schema=resource.descriptor,
+        )
+        reports.append(report)
+    f = open(report_name, 'w')
+    json.dump(reports, f)
+
+generate_report(datapackage)
+
+dp_report = {
+    'name': datapackage['name']+'-report',
+    'resources': [{
+        PROP_STREAMED_FROM: report_name,
+        'name': 'datapackage_report',
+        'format': 'json',
+        'path': 'data/datapackage_report.json',
+        'datahub': {
+          'type': "derived/report",
+        },
+        'description': 'Validation report for tabular data'
+    }]
+}
+
+
+spew(dp_report, [])
